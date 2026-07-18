6ba90e8dbddeb74509103ea46d54dd97e838ad60
USER <user@MacBook-Pro-Dam.local>
2026-07-18 16:11:44 +0800

    welcome to the OOP_Project

---
+++ WebApplication1/Data/MyAppContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data;
+
+public class MyAppContext: DbContext
+{
+    public MyAppContext(DbContextOptions<MyAppContext> options) : base(options)
+    {
+                
+    }
+    
+    public DbSet<Item> Items { get; set; }
+}
